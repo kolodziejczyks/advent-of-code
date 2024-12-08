53b5a26abe0cd092f1f42c0364b94e1bb6629f00
Szymon Kołodziejczyk <szy.kolodziejczyk@gmail.com>
2024-12-08 16:52:21 +0100

    feat: the whole function with unit tests

--- problems/1/day1.spec.ts
+++ problems/1/day1.spec.ts
@@ -1,21 +1,45 @@
 import { Day1 } from "./day1";
 
 let day: Day1;
+let arr1: Array<number>;
+let arr2: Array<number>;
 
 const setup = (): void => {
-  const arr1 = [3, 4, 2, 1, 3, 3];
-  const arr2 = [4, 3, 5, 3, 9, 3];
-
-  day = new Day1(arr1, arr2);
+  day = new Day1();
+  arr1 = [3, 4, 2, 1, 3, 3];
+  arr2 = [4, 3, 5, 3, 9, 3];
 };
 
 describe("Day1", () => {
-  it("initializes successfully", () => {
+  beforeEach(() => {
     setup();
+  });
+
+  it("initializes successfully", () => {
     expect(day).toBeTruthy();
   });
 
-  it('', () => {
-    
-  })
+  it("finds smallest numbers for both arrays", () => {
+    const small1 = day.findAndDeleteSmallestNumber(arr1);
+    const small2 = day.findAndDeleteSmallestNumber(arr2);
+
+    expect(small1).toEqual(1);
+    expect(small2).toEqual(3);
+  });
+
+  it("returs correct distance betweeen numbers", () => {
+    const distance1 = day.calculateDistance(3, 3);
+    const distance2 = day.calculateDistance(1, 4);
+
+    expect(distance1).toEqual(0);
+    expect(distance2).toEqual(3);
+  });
+
+  it("makes calculation for first number and then goes to another", () => {
+    expect(day.iterateOverLists(arr1, arr2)).toEqual([2, 1, 0, 1, 2, 5]);
+  });
+
+  it("sums up the whole array", () => {
+    expect(day.sumResults(day.iterateOverLists(arr1, arr2))).toEqual(11);
+  });
 });
--- problems/1/day1.ts
+++ problems/1/day1.ts
@@ -1,3 +1,45 @@
 export class Day1 {
-  constructor(list1: Array<number>, list2: Array<number>) {}
+  public findAndDeleteSmallestNumber(arr: Array<number>): number | undefined {
+    arr.sort();
+
+    if (arr.length === 1) {
+      const result = arr[0];
+      arr.pop();
+      return result;
+    }
+
+    return arr.shift();
+  }
+
+  public calculateDistance(num1, num2): number {
+    return Math.abs(num1 - num2);
+  }
+
+  public iterateOverLists(
+    arr1: Array<number>,
+    arr2: Array<number>
+  ): Array<number> {
+    let results: Array<number> = [];
+    const fullLength = arr1.length;
+
+    if (arr1.length !== arr2.length) {
+      throw new Error("Length not equal! Check the input arrays.");
+    }
+
+    for (let i = 0; i < fullLength; i++) {
+      let j1 = this.findAndDeleteSmallestNumber(arr1);
+      let j2 = this.findAndDeleteSmallestNumber(arr2);
+
+      results.push(this.calculateDistance(j1, j2));
+    }
+
+    return results;
+  }
+
+  public sumResults(array): number {
+    return array.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0,
+    );
+  }
 }
